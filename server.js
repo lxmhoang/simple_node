d2056a1cf890c69513b8158a309796c20a86eca3
Hoang Le <lxmhoang@gmail.com>
2022-11-28 15:17:48 +0700

    FIX syntax

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ async function main() {
   while(true) {
     console.log('Containers rule!');
     await sleep(2500);
-    console.log('my fav food : ${fav_food}');
-    await sleep(2500);
+    console.log(`my fav food : ${fav_food}`);
+    await sleep(2500); 
   }
 }
 
