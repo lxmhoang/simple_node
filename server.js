fdc46e2caa76fb61e596bb2d70d58050a8f131d7
Hoang Le <lxmhoang@gmail.com>
2022-11-28 14:42:07 +0700

    print var env

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ function sleep(ms) {
 async function main() {
   while(true) {
     console.log('Containers rule!');
+    console.log('my fav food : ${process.env.FAVFOOD}');
     await sleep(5000);
   }
 }
